8137b485e9598b13dcffd92124d40c77e8360148
Micah Lee <micah.lee@cyberark.com>
2019-02-20 08:20:27 -0500

    Update app, policy, and demo scripts for new entitlement flow

--- app/test_app.rb
+++ app/test_app.rb
@@ -9,8 +9,18 @@ class TestApp < Sinatra::Application
   get '/' do
     "
       <h1>Visit us @ www.conjur.org!</h1>
-      <p>Database Username: #{ENV['DB_USERNAME']}</p>
-      <p>Database Password: #{ENV['DB_PASSWORD']}</p>
+
+      <h3>Org-wide Secrets</h3>
+      <p>Database Username: #{ENV['ORG_DB_USERNAME']}</p>
+      <p>Database Password: #{ENV['ORG_DB_PASSWORD']}</p>
+
+      <h3>Space-wide Secrets</h3>
+      <p>Database Username: #{ENV['SPACE_DB_USERNAME']}</p>
+      <p>Database Password: #{ENV['SPACE_DB_PASSWORD']}</p>
+
+      <h3>App-specific Secrets</h3>
+      <p>Database Username: #{ENV['APP_DB_USERNAME']}</p>
+      <p>Database Password: #{ENV['APP_DB_PASSWORD']}</p>
       <p>Stripe API Key: #{ENV['STRIPE_API_KEY']}</p>
     "
   end
