bfe4eb370a77cb9b3edd6efca2c389832a079046
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2020-12-08 00:01:01 +0000

    Change test app to use Golang for tas-with-installed-tile
    The ruby app has been consistently failing with errors of the form 'Unable to determine ruby: unable to determine ruby engine'. The Golang buildpack has been a much simpler exercise.

---
+++ tas-with-installed-tile/app/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func main() {
+	log.SetFormatter(&log.TextFormatter{DisableTimestamp: true, DisableLevelTruncation: true})
+	log.SetLevel(log.DebugLevel)
+
+	log.Infof("Environment:\n%s\n\n", strings.Join(os.Environ(), "\n"))
+
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		log.Infof("Handling request\n")
+		res.WriteHeader(200)
+
+		var secret = "[envvar not found]"
+		if secretValue, ok := os.LookupEnv("DEMO_SECRET"); ok {
+			secret = secretValue
+		}
+
+		_, _ = fmt.Fprintf(res, `
+<h1>Visit us @ www.conjur.org!</h1>
+<p>App secret: %s</p>
+`, secret)
+		log.Infof("Handled request\n")
+	})
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		port = "8080"
+	}
+
+	log.Infof("Listening at port %s...", port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Errorf("Server error: %s\n", err.Error())
+		os.Exit(1)
+	}
+}
